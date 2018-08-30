b3cfcfe28fe927dbf8035340ddd127d851879a03
Hyeongsun <fdjkp@naver.com>
2018-08-30 02:32:13 +0000

    echo bot

--- README.md
+++ README.md
@@ -35,5 +35,13 @@ def keyboard():
     
 # 서버 실행 코드    
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
-
 ```
+
+### API
+- request
+    - URL : 어떤 경로로 보낼꺼니?
+    - method : 어떤 방법으로 보낼꺼니?
+    - parameter : 어떤 정보를 담을꺼니?
+- response
+    - data type : 어떤 형식으로 답할까?
+    - 
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -10,7 +10,6 @@ def hello():
     
 @app.route('/keyboard')
 def keyboard():
-    
     # keyboard 딕셔너리 생성
     keyboard = {
         "type" : "buttons",
@@ -21,9 +20,25 @@ def keyboard():
     json_keyboard = json.dumps(keyboard)
     return json_keyboard
 
-# POST방식으로 URL에 안 찍히도록    
-# @app.route('/message', methods=['POST'])
 
+# POST방식으로 URL에 안 찍히도록    
+@app.route('/message', methods=['POST'])
+def message():
+    # content라는 key의 value를 msg에 json타입으로 저장
+    msg = request.json['content']
+    
+    json_return = {
+        "message":{
+            "text" : msg
+        },
+        "keyboard": {
+            "type" : "buttons",
+            "buttons" : ["메뉴", "로또", "고양이", "영화"]
+        }
+    }
+    
+    # jsonify도 json.dumps와 똑같은 기능을 한다. 취사선택하면 됨.
+    return jsonify(json_return)
     
 # 서버 실행 코드    
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
