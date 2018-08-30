b329a7252e83a50d946c0ff76167a82de0063abb
Hyeongsun <fdjkp@naver.com>
2018-08-30 07:16:52 +0000

    final commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import json
 import random
 import requests
+from bs4 import BeautifulSoup
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -42,15 +43,51 @@ def message():
         img_bool = True
         url = "https://api.thecatapi.com/v1/images/search?mime_type=jpg"
         req = requests.get(url).json()
-        cat_url = req[0]['url']
+        return_msg = "나만 고양이 없어 ㅜㅜ"
+        img_url = req[0]['url']
+    elif msg == "영화":
+        img_bool = True
+        url = "https://movie.naver.com/movie/running/current.nhn"
+        req = requests.get(url).text
+        doc = BeautifulSoup(req, 'html.parser')
+        
+        # class가 tit인 dt 태그 아래에 있는 a 태그만 보고싶어(단 중간경로도 있으면 다 적어줘야한다.)
+        titles = doc.select('dt.tit > a')
+        
+        # 평점 가져오기(copy selector 해온 것 중 div:nth-child(1)와 같이 자식 구조가 있을 때 div만 남기고 빼준다.)
+        stars = doc.select('#content > div.article > div > div.lst_wrap > ul > li > dl > dd.star > dl.info_star > dd > div > a > span.num')
+        
+        # 예매율 가져오기
+        reserve = doc.select('#content > div.article > div > div.lst_wrap > ul > li > dl > dd.star > dl.info_exp > dd > div > span.num')
+        
+        # 영화 포스터 이미지 가져오기
+        image = doc.select('div.thumb > a > img')
+        
+        # TOP10 영화 딕셔너리 만들기
+        movie_dic = {}
+        for i in range(0,10):
+            movie_dic[i] = {
+                "title": titles[i].text,
+                "star": stars[i].text,
+                "reserve": reserve[i].text,
+                # img태그 안의 src 속성 부분만 가져오기
+                "img": image[i].get('src')
+            }
+        
+        # 하나의 영화만 pick해주기
+        pick_movie = movie_dic[random.randrange(0,10)]
+        
+        return_msg = "%s / 평점: %s / 예매율: %s" %(pick_movie['title'],pick_movie['star'],pick_movie['reserve'])
+        img_url = pick_movie['img']
     else:
         return_msg = "아직 지원하지 않습니다."
         
     if img_bool == True:
         json_return = {
             "message":{
+                "text": return_msg,
                 "photo": {
-                    "url": cat_url,
+                    "url": img_url,
                     "width": 720,
                     "height": 640
                 }
